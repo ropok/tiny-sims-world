5f1be78542044156f5076fc9c1f6b00e4ca74b64
ropok <jalerse@gmail.com>
2022-09-24 22:46:36 +0700

    ClickInteract: Basic Object Click
    - InteractableObject are clickable
    - Click object and Log the name, to test purpose

---
+++ Tiny Sims World/Assets/Scripts/ClickInteract.cs
@@ -0,0 +1,39 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace TinySimsWorld
+{
+    public class ClickInteract : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            var clickedGameObject = GetClickableGameObject();
+            if (clickedGameObject == null) return;
+            if (Input.GetMouseButton(0))
+            {
+                var interactable = clickedGameObject.transform.name.ToString();
+                Debug.Log(interactable);
+            }
+        }
+
+        private GameObject GetClickableGameObject()
+        {
+            GameObject result = null;
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, Vector2.zero);
+            if (hitInfo)
+            {
+                result = hitInfo.transform.gameObject;
+            }
+
+            return result;
+        }
+    }
+}
