30f8d46541561ea198a1fee4a44e6a74482255c8
ropok <jalerse@gmail.com>
2022-09-26 22:52:37 +0700

    Art: Add walking animation character
    - Player walking animation character
    - Player animation walk up down left right

--- Tiny Sims World/Assets/Scripts/Player/Player.cs
+++ Tiny Sims World/Assets/Scripts/Player/Player.cs
@@ -5,9 +5,13 @@ namespace TinySimsWorld.Player
     public class Player : MonoBehaviour
     {
         [SerializeField] private PlayerSettings playerSettings;
+        [SerializeField] private Animator animator;
 
         private IPlayerInput _playerInput;
         private PlayerMovement _playerMovement;
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
+        private static readonly int Magnitude = Animator.StringToHash("Magnitude");
 
         private void Awake()
         {
@@ -19,6 +23,12 @@ namespace TinySimsWorld.Player
         {
             _playerInput.ReadInput();
             _playerMovement.Move();
+            
+            animator.SetFloat(Horizontal, _playerInput.Speed.x);
+            animator.SetFloat(Vertical, _playerInput.Speed.y);
+            animator.SetFloat(Magnitude, _playerInput.Speed.magnitude);
+            
         }
+
     }
 }
